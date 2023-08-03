4161b12581be197c89fbcb663d724fce1261a327
Anand Naik <anandbukyanaik9573@gmail.com>
2023-08-03 23:23:23 +0530

    create middlewares and app.listen server

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app=express();
+
+app.use((req,res,next)=>{
+    console.log('In the middleware');
+    next();
+});
+
+app.use((req,res,next)=>{
+    console.log('In another middleware');
+    res.send("<h1>Hello from Express!</h1>")
+    res.send('{ key1: value }') 
+});
+
+app.listen(4000);
