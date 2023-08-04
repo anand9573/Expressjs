6dcf6a8fc6e21809e6b7446aefe0fa7126cff255
Anand Naik <anandbukyanaik9573@gmail.com>
2023-08-04 11:34:58 +0530

    adding product and send response

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require('express');
-
+const bodyparser=require('body-parser');
 const app=express();
 
-app.use((req,res,next)=>{
-    console.log('In the middleware');
+app.use(bodyparser.urlencoded({extended:false}));
+
+app.use('/',(req,res,next)=>{
+    console.log('this always runs!')
     next();
 });
 
-app.use((req,res,next)=>{
-    console.log('In another middleware');
-    res.send("<h1>Hello from Express!</h1>")
-    res.send('{ key1: value }') 
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action="/product" method="POST"><input type="text" name="product" placeholder="product name">&nbsp;<input type="number" name="input_size" placeholder="product size">&nbsp;<button type="submit">Add Product</button></form>');
+});
+    app.use('/product',(req,res,next)=>{
+        res.send(`<h3>Product Name: ${req.body.product}<br> Product size : ${req.body.input_size}</h3>`)
+    });
+app.use('/',(req,res,next)=>{
+    console.log('program ended!')
 });
 
 app.listen(4000);
