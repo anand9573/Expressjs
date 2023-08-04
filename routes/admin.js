a84330dde7107da0bca43ce37dab4c12f1835573
Anand Naik <anandbukyanaik9573@gmail.com>
2023-08-04 13:12:58 +0530

    routing and filtering

---
+++ routes/admin.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const router=express.Router();
+
+router.get('/add-product',(req,res,next)=>{
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="product" placeholder="product name">&nbsp;<button type="submit">Add Product</button></form>');
+});
+router.post('/add-product',(req,res,next)=>{
+console.log(req.body);
+res.redirect('/shop')
+});
+
+module.exports=router;
---
+++ routes/shop.js
@@ -0,0 +1,7 @@
+const express=require('express');
+const router=express.Router();
+
+router.get('/',(req,res,next)=>{
+    res.send('<h1>Hello from Expressjs!</h1>')
+})
+module.exports=router;
